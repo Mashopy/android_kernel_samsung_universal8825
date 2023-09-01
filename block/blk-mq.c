43cda676f3473a6039e2e7ef456305a7127bdf92
Mashopy <eliasgheeraert@gmail.com>
2023-09-01 05:07:25 +0200

    block: Import SM-A536B changes from A536BXXU5CWD1

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -680,14 +680,16 @@ bool blk_mq_complete_request_remote(struct request *rq)
 		rq->csd.func = __blk_mq_complete_request_remote;
 		rq->csd.info = rq;
 		rq->csd.flags = 0;
-		smp_call_function_single_async(rq->mq_ctx->cpu, &rq->csd);
-	} else {
-		if (rq->q->nr_hw_queues > 1)
-			return false;
-		blk_mq_trigger_softirq(rq);
+		if (!smp_call_function_single_async(rq->mq_ctx->cpu, &rq->csd))
+			return true;
 	}
 
-	return true;
+	if (rq->q->nr_hw_queues == 1) {
+		blk_mq_trigger_softirq(rq);
+		return true;
+	}
+
+	return false;
 }
 EXPORT_SYMBOL_GPL(blk_mq_complete_request_remote);
 
